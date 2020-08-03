efc013e71ab629606a0073e4373a48670d7815d3
Jim Hill <jim.hill.r@gmail.com>
2020-08-03 15:16:40 -0500

    Updated description

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Jim & Meagan`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Our adventure beings in 2021`,
     author: `@gatsbyjs`,
   },
   plugins: [
